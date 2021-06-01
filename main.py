7f03d7c77fcba1f1a0bb2392fc60ae9dd35774b4
InsecureRada <>
2021-06-01 08:50:27 +0000

    bug fix

--- main.py
+++ main.py
@@ -136,62 +136,63 @@ async def on_message(message):
       mult = message.content.split("$$mcalc ",1)[1]
       if float(mult) > 1000.95:
         await message.channel.send('The $$mcalc command is capped to only calculating multipliers of 1000.95x and below!')
-      pPoints = 10000000000000
-      minDict = {
-        .05: 10,
-        .15: 50,
-        .3: 250,
-        .5: 750,
-        1: 2500
-      }
-      beforeDict = {
-        .05: 10,
-        .15: 50,
-        .3: 250,
-        .5: 750,
-        1: 2500
-      }
-      boughtDict = {
-        .05: 0,
-        .15: 0,
-        .3: 0,
-        .5: 0,
-        1: 0
-      }
-      divDict = {
-        .05: 0, 
-        .15: 0,
-        .3: 0,
-        .5: 0,
-        1: 0
-      }
-      bestToBuy = .05
-      mult = float(mult)
-      while pPoints > 0:
-        if round(boughtDict[.05] * .05 + boughtDict[.15] * .15 + boughtDict[.3] * .3 + boughtDict[.5] * .5 + boughtDict[1] + 1, 3) >= mult:
-          divDict[bestToBuy] = 0
-          bestToBuy = (max(divDict, key=divDict.get))
-          if all(value == 0 for value in divDict.values()):
-            break
-        elif round(boughtDict[.05] * .05 + boughtDict[.15] * .15 + boughtDict[.3] * .3 + boughtDict[.5] * .5 + boughtDict[1] + 1 + bestToBuy, 3) > mult:
-          divDict[bestToBuy] = 0
-          bestToBuy = (max(divDict, key=divDict.get))
-          if all(value == 0 for value in divDict.values()):
-            break
-        # elif boughtDict[bestToBuy] >= 1001:
-          # divDict[bestToBuy] = 0
-          # bestToBuy = (max(divDict, key=divDict.get))
-          # if all(value == 0 for value in divDict.values()):
-            # break
-        elif pPoints - minDict[bestToBuy] >= 0:
-          pPoints = pPoints - minDict[bestToBuy]
-          minDict[bestToBuy] += beforeDict[bestToBuy]
-          boughtDict[bestToBuy] += 1
-          for i, v in minDict.items():
-            divDict[i] = i/v
-          bestToBuy = (max(divDict, key=divDict.get))
-      totalMult = round(boughtDict[.05] * .05 + boughtDict[.15] * .15 + boughtDict[.3] * .3 + boughtDict[.5] * .5 + boughtDict[1] + 1, 3)
-      await message.channel.send('5% Multipliers: ' + str(boughtDict.get(.05)) + '\n15% Multipliers: ' + str(boughtDict.get(.15)) + '\n30% Multipliers: ' + str(boughtDict.get(.30)) + '\n50% Multipliers: ' + str(boughtDict.get(.50)) + '\n100% Multipliers: ' + str(boughtDict.get(1)) + '\n\nYou will need ' + str(10000000000000 - pPoints) + ' prestige points to get a ' + str(totalMult) + 'x multiplier.')
+      else:
+        pPoints = 10000000000000
+        minDict = {
+          .05: 10,
+          .15: 50,
+          .3: 250,
+          .5: 750,
+          1: 2500
+        }
+        beforeDict = {
+          .05: 10,
+          .15: 50,
+          .3: 250,
+          .5: 750,
+          1: 2500
+        }
+        boughtDict = {
+          .05: 0,
+          .15: 0,
+          .3: 0,
+          .5: 0,
+          1: 0
+        }
+        divDict = {
+          .05: 0, 
+          .15: 0,
+          .3: 0,
+          .5: 0,
+          1: 0
+        }
+        bestToBuy = .05
+        mult = float(mult)
+        while pPoints > 0:
+          if round(boughtDict[.05] * .05 + boughtDict[.15] * .15 + boughtDict[.3] * .3 + boughtDict[.5] * .5 + boughtDict[1] + 1, 3) >= mult:
+            divDict[bestToBuy] = 0
+            bestToBuy = (max(divDict, key=divDict.get))
+            if all(value == 0 for value in divDict.values()):
+              break
+          elif round(boughtDict[.05] * .05 + boughtDict[.15] * .15 + boughtDict[.3] * .3 + boughtDict[.5] * .5 + boughtDict[1] + 1 + bestToBuy, 3) > mult:
+            divDict[bestToBuy] = 0
+            bestToBuy = (max(divDict, key=divDict.get))
+            if all(value == 0 for value in divDict.values()):
+              break
+          # elif boughtDict[bestToBuy] >= 1001:
+            # divDict[bestToBuy] = 0
+            # bestToBuy = (max(divDict, key=divDict.get))
+            # if all(value == 0 for value in divDict.values()):
+              # break
+          elif pPoints - minDict[bestToBuy] >= 0:
+            pPoints = pPoints - minDict[bestToBuy]
+            minDict[bestToBuy] += beforeDict[bestToBuy]
+            boughtDict[bestToBuy] += 1
+            for i, v in minDict.items():
+              divDict[i] = i/v
+            bestToBuy = (max(divDict, key=divDict.get))
+        totalMult = round(boughtDict[.05] * .05 + boughtDict[.15] * .15 + boughtDict[.3] * .3 + boughtDict[.5] * .5 + boughtDict[1] + 1, 3)
+        await message.channel.send('5% Multipliers: ' + str(boughtDict.get(.05)) + '\n15% Multipliers: ' + str(boughtDict.get(.15)) + '\n30% Multipliers: ' + str(boughtDict.get(.30)) + '\n50% Multipliers: ' + str(boughtDict.get(.50)) + '\n100% Multipliers: ' + str(boughtDict.get(1)) + '\n\nYou will need ' + str(10000000000000 - pPoints) + ' prestige points to get a ' + str(totalMult) + 'x multiplier.')
     except:
       await message.channel.send('Please send a valid multiplier. (ex: $$mcalc 1000)')
 
